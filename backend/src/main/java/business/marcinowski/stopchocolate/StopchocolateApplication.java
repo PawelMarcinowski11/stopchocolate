eee127657752377c1128ab5a4156178d66a830d7
Paweł Marcinowski <pawel.marcinowski11@gmail.com>
2024-12-01 22:11:20 +0100

    first commit

---
+++ backend/src/main/java/business/marcinowski/stopchocolate/StopchocolateApplication.java
@@ -0,0 +1,13 @@
+package business.marcinowski.stopchocolate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopchocolateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StopchocolateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/business/marcinowski/stopchocolate/StopchocolateApplicationTests.java
@@ -0,0 +1,13 @@
+package business.marcinowski.stopchocolate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopchocolateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
